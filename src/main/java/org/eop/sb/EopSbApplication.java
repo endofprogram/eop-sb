bbc1c36ba8ee20baa36e4cf2897bf33284bff163
Administrator <Administrator@L8Y9K64OI1IX0G4>
2018-09-01 22:14:49 +0800

    init

---
+++ src/main/java/org/eop/sb/EopSbApplication.java
@@ -0,0 +1,15 @@
+package org.eop.sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @author lixinjie
+ * @since 2018-09-01
+ */
+@SpringBootApplication
+public class EopSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EopSbApplication.class, args);
+	}
+}
